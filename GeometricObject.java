e260731ea8eee37cb01815a631c3e93893ecc547
LiongsonEnzo <156958405+LiongsonEnzo@users.noreply.github.com>
2024-09-10 23:19:25 -0400

    Create GeometricObject.java

---
+++ GeometricObject.java
@@ -0,0 +1,12 @@
+public abstract class GeometricObject {
+    // Constructor
+    protected GeometricObject() {
+        // Default constructor
+    }
+    
+    // Abstract method to get area
+    public abstract double getArea();
+    
+    // Abstract method to get perimeter
+    public abstract double getPerimeter();
+}
